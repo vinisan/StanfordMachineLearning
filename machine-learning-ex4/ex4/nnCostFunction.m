a7be3b015d8f7d634349cb6e16e052d67f423bfb
vinisan <IDanalytics@Arth.local>
2015-10-19 10:39:14 -0700

    Putting all the assignments together

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,44 +62,6 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-a1 = [ones(m, 1) X];
-z2 = a1 * Theta1';
-a2 = sigmoid(z2);
-a2 = [ones(m, 1) a2];
-z3 = a2 * Theta2';
-a3 = sigmoid(z3);
-
-for k = 1:num_labels
-    yk = y == k;
-    hthetak = a3(:, k);
-    left_side = -yk .* log(hthetak);
-    right_side = (1 - yk) .* log(1 - hthetak);
-    Jk = 1 / m * sum(left_side - right_side);
-    J = J + Jk;
-end
-
-reg = lambda / (2 * m) * (sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 2:end) .^ 2)));
-J = J + reg;
-
-
-for t = 1:m
-    for k = 1:num_labels
-        yk = y(t) == k;
-        delta_3(k) = a3(t, k) - yk;
-    end
-    delta_2 = Theta2' * delta_3' .* sigmoidGradient([1, z2(t, :)])';
-    delta_2 = delta_2(2:end);
-
-    Theta1_grad = Theta1_grad + delta_2 * a1(t, :);
-    Theta2_grad = Theta2_grad + delta_3' * a2(t, :);
-end
-
-Theta1_grad = Theta1_grad / m;
-Theta2_grad = Theta2_grad / m;
-
-
-Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + lambda / m * Theta1(:, 2:end);
-Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + lambda / m * Theta2(:, 2:end);
 
 
 
